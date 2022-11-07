847d95dbbc7e81748f74ccdf90d368bdd94ade36
Kirill Bystrov <kir9aza@gmail.com>
2022-11-07 17:59:19 +0300

    create form post request

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Form post request</title>
+</head>
+<body>
+	<form action="login.php" method = 'post'>
+		<label for="login">Логин:</label>
+		<input type="text" name = 'login'>
+		<label for="password">Пароль:</label>
+		<input type="text" name = "password">
+	</form>
+</body>
+</html>
