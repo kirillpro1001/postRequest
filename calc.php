00da9cac11580bdb8c5578ac805c795ea77ea516
Kirill Bystrov <kir9aza@gmail.com>
2022-11-07 19:34:56 +0300

    check for a number

--- calc.php
+++ calc.php
@@ -2,6 +2,9 @@
 	$numberOne = $_GET['numberOne'];
 	$numberTwo = $_GET['numberTwo'];
 	$action = $_GET['action'];
+	$arrNumberOne = str_split($numberOne,1);
+	$arrNumberTwo = str_split($numberTwo,1);
+
 
 	if (!isset($_GET)) {
 		return "Значения не переданы";
@@ -12,6 +15,12 @@
 	if (!isset($numberOne) || !isset($numberTwo)) {
 		return "Не переданы аргументы";
 	}
+	if ($arrNumberOne[0] == " " || $arrNumberTwo[0] == " ") {
+		return "Уберите лишние пробелы";
+	}
+
+	if (is_numeric($numberOne) && is_numeric($numberTwo)) {
+		
 
 	$calculation = $numberOne . ' '.$_GET['action'].' '.$numberTwo.' = ';
 
@@ -34,4 +43,8 @@
 	}
 
 	return $calculation.$resultCalc;
+}
+else {
+	return "Ошибка. В качестве аргумента передана строка";
+}
 ?>
